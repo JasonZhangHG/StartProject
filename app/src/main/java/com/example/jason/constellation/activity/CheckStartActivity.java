e87593b9f526bfa2a14184c1b46940f1fa4ff133
张海龙 <381266395@qq.com>
2018-03-02 15:26:12 +0800

    checkActivity add back

--- app/src/main/java/com/example/jason/constellation/activity/CheckStartActivity.java
+++ app/src/main/java/com/example/jason/constellation/activity/CheckStartActivity.java
@@ -1,15 +1,29 @@
 package com.example.jason.constellation.activity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.example.jason.constellation.R;
 import com.example.jason.constellation.base.BaseActivity;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class CheckStartActivity extends BaseActivity {
+
+    @BindView(R.id.check_title_back) ImageView checkTitleBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_check_start2);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.check_title_back)
+    public void onBackClicked(View view) {
+        finish();
     }
 }
