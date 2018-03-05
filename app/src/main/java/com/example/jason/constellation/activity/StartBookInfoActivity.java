93cc04e49e493cdd7a8cac4e4fed34e74edd295d
张海龙 <381266395@qq.com>
2018-03-05 11:43:21 +0800

    change gradle version from 19 to 15

--- app/src/main/java/com/example/jason/constellation/activity/StartBookInfoActivity.java
+++ app/src/main/java/com/example/jason/constellation/activity/StartBookInfoActivity.java
@@ -24,7 +24,7 @@ public class StartBookInfoActivity extends BaseActivity {
     @BindView(R.id.tv_start_music) TextView tvMusic;
 
     private String[] name =
-            {"白羊座", "金牛座", "双子座",
+                    {"白羊座", "金牛座", "双子座",
                     "巨蟹座", "狮子座", "处女座",
                     "天秤座", "天蝎座", "射手座",
                     "魔蝎座", "水瓶座", "双鱼座"};
